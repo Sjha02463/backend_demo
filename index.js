c0a2efaf40bf12eeaab8a71d7e89501e3df515bf
Sjha02463 <jha317733@gmail.com>
2025-06-16 12:23:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/twitter', (req,res) => {res.send('twitter') })
-app.get('/login', (req,res) => {res.send('<h1>Thisis the login page<h1>')})
+app.get('/login', (req,res) => {res.send('<h1>Thisis the login page</h1>')})
 app.get('/youtube', (req,res) =>{res.send('<h2>Backend code</h2>')} )
 
 app.listen(process.env.PORT, () => {
